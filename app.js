e46080fa8d9ae267edf8961f718f7494cc234dcf
Anna Manole <am224wd@student.lnu.se>
2023-08-28 09:39:00 +0200

    Hide message as well

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+const message = document.getElementById('message')
 const button = document.getElementById('btn')
 const input = document.getElementById('name')
 const greeting = document.getElementById('greet')
 
 button.addEventListener('click', () => {
-  // add the hidden class to the input and button
+  // add the hidden class to the message, input and button
+  message.classList.add('hidden')
   input.classList.add('hidden')
   button.classList.add('hidden')
   
   // remove the hidden class from the greeting
   greeting.classList.remove('hidden')
-  
+
   // append the name to the greeting
   const name = input.value
   greeting.textContent += ` ${name}!`
