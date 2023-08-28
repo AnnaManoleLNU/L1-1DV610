a58a944448084243e46458453043b329754154b0
Anna Manole <am224wd@student.lnu.se>
2023-08-28 12:49:18 +0200

    Generate random images from unsplash for the user

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const button = document.getElementById('btn')
 const input = document.getElementById('name')
 const greeting = document.getElementById('greet')
 
+// Function to generate a random image from the Unsplash API
+async function generateImage() {
+  // Get the image from the API
+  // only get images that are horizontal
+  const response = await fetch('https://source.unsplash.com/featured/1920x1080/?nature,minimalism')
+  return response.url
+}
+
 // Function to handle the event
-function handleEvent() {
+async function handleEvent() {
   // add the hidden class to the message, input, and button
   message.classList.add('hidden')
   input.classList.add('hidden')
@@ -15,13 +23,20 @@ function handleEvent() {
 
   // append the name to the greeting
   const name = input.value
-  greeting.textContent += ` ${name}!`
+  greeting.textContent += ` ${name}! Here is your picture of the day!`
+
+  // append the image to the div
+  const image = document.createElement('img')
+  image.src = await generateImage()
+  const div = document.getElementById('imagegoeshere')
+  div.appendChild(image)
 }
+
 
 // Event listener for Enter key press
 input.addEventListener('keypress', function (event) {
   if (event.key === 'Enter') {
-    event.preventDefault();
+    event.preventDefault()
     handleEvent() // Call the event handling function
   }
 })
