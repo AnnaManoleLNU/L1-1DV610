37089cd9597e5a392c04cf7217685c17690bf018
Anna Manole <am224wd@student.lnu.se>
2023-08-28 09:45:26 +0200

    Trigger the event with both enter and mouse click

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const button = document.getElementById('btn')
 const input = document.getElementById('name')
 const greeting = document.getElementById('greet')
 
-button.addEventListener('click', () => {
-  // add the hidden class to the message, input and button
+// Function to handle the event
+function handleEvent() {
+  // add the hidden class to the message, input, and button
   message.classList.add('hidden')
   input.classList.add('hidden')
   button.classList.add('hidden')
-  
+
   // remove the hidden class from the greeting
   greeting.classList.remove('hidden')
 
   // append the name to the greeting
   const name = input.value
   greeting.textContent += ` ${name}!`
-  
+}
+
+// Event listener for Enter key press
+input.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    handleEvent() // Call the event handling function
+  }
 })
+
+// Event listener for button click
+button.addEventListener('click', function () {
+  handleEvent() // Call the event handling function
+});
